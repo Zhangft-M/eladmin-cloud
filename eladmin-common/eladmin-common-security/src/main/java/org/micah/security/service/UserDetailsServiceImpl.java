2df85fe7fea006fac11eac7e68423c9a8054388a
张方涛 <767602601@qq.com>
2020-08-12 20:05:46 +0800

    测试获取菜单权限之前

--- eladmin-common/eladmin-common-security/src/main/java/org/micah/security/service/UserDetailsServiceImpl.java
+++ eladmin-common/eladmin-common-security/src/main/java/org/micah/security/service/UserDetailsServiceImpl.java
@@ -1,16 +1,16 @@
 package org.micah.security.service;
 
-import cn.hutool.core.util.ArrayUtil;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.system.UserInfo;
-import lombok.RequiredArgsConstructor;
+import cn.hutool.core.collection.CollUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.micah.core.constant.CacheConstants;
 import org.micah.core.constant.SecurityConstants;
+import org.micah.model.dto.SysUserDto;
 import org.micah.security.component.LoginUser;
+import org.micah.security.config.LoginProperties;
+import org.micah.sysapi.api.IRemoteMenuService;
+import org.micah.sysapi.api.IRemoteUserService;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -18,9 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -33,71 +31,91 @@ import java.util.Set;
  **/
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
 
-    /* *//**
-     * feign接口
-     *//*
-    private final RemoteUserService remoteUserService;
 
-    *//**
-     * 缓存管理器，在第一次调用loadUserByUsername之后会把用户信息给缓存
-     *//*
+    /**
+     * userfeign接口
+     */
+    private final IRemoteUserService remoteUserService;
+
+
+    private final IRemoteMenuService remoteMenuService;
+
+
+    /**
+     * 缓存管理器，在第一次调用加载用户的方法系统会自动将其缓存
+     */
     private final CacheManager cacheManager;
 
-    *//**
-     * 用户密码登录，远程调用feign客户端加载用户信息
-     * @param username 用户名
+    /**
+     * 用户登录设置
+     */
+    private final LoginProperties loginProperties;
+
+    public UserDetailsServiceImpl(IRemoteUserService remoteUserService,
+                                  IRemoteMenuService remoteMenuService, CacheManager cacheManager, LoginProperties loginProperties) {
+        this.remoteUserService = remoteUserService;
+        this.remoteMenuService = remoteMenuService;
+        this.cacheManager = cacheManager;
+        this.loginProperties = loginProperties;
+    }
+
+
+    /**
+     * 设置用户信息是否被缓存
+     * @param enableCache
+     */
+    public void setEnableCache(boolean enableCache) {
+        this.loginProperties.setCacheEnable(enableCache);
+    }
+
+    /**
+     * 通过用户名加载用户
+     * @param username
      * @return
      * @throws UsernameNotFoundException
-     *//*
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Cache cache = this.cacheManager.getCache(CacheConstants.USER_DETAILS);
-        if (cache!=null && cache.get(username)!=null){
+        if (cache != null && cache.get(username) != null) {
             // 缓存中存在，直接从缓存中获取
-            return (LoginUser)cache.get(username).get();
+            return (LoginUser) cache.get(username).get();
         }
         // 缓存不存在则调用feign获取用户信息
-        ResponseEntity<UserInfo> result = this.remoteUserService.getUserInfo(username);
+        ResponseEntity<SysUserDto> result = this.remoteUserService.queryByUsername(username);
         UserDetails userDetails = this.getUserDetails(result);
         // 放入缓存
-        cache.put(username,userDetails);
+        if (loginProperties.isCacheEnable()) {
+            assert cache != null;
+            cache.put(username, userDetails);
+        }
         return userDetails;
     }
 
-    *//**
-     * 构造UserDetails
+
+    /**
+     * 构建UserDetails
      * @param result
      * @return
-     *TODO: 2020/8/5 在系统用户功能处理完毕再处理
-     *//*
-    private UserDetails getUserDetails(ResponseEntity<UserInfo> result) {
+     */
+    private UserDetails getUserDetails(ResponseEntity<SysUserDto> result) {
         if (result == null || result.getBody() == null) {
             throw new UsernameNotFoundException("用户不存在");
         }
-        UserInfo info = result.getBody();
+        SysUserDto userDto = result.getBody();
         Set<String> dbAuthsSet = new HashSet<>();
-        if (ArrayUtil.isNotEmpty(info.getRoles())) {
-            // 获取角色
-            Arrays.stream(info.getRoles()).forEach(role -> dbAuthsSet.add(SecurityConstants.ROLE + role));
-            // 获取资源
-            dbAuthsSet.addAll(Arrays.asList(info.getPermissions()));
+        if (CollUtil.isNotEmpty(userDto.getRoles())) {
+            userDto.getRoles().forEach(role -> {
+                dbAuthsSet.add(SecurityConstants.ROLE + role.getName());
+            });
 
         }
-        Collection<? extends GrantedAuthority> authorities = AuthorityUtils
+        Collection<GrantedAuthority> authorities = AuthorityUtils
                 .createAuthorityList(dbAuthsSet.toArray(new String[0]));
-        SysUser user = info.getSysUser();
+        return new LoginUser(userDto.getUsername(), userDto.getPassword(), userDto.getEnabled(),
+                true, true, true, authorities, userDto, null);
 
-        // 构造security用户
-        return new PigUser(user.getUserId(), user.getDeptId(), user.getUsername(),
-                SecurityConstants.BCRYPT + user.getPassword(),
-                StrUtil.equals(user.getLockFlag(), CommonConstants.STATUS_NORMAL), true, true, true, authorities);
-
-    }*/
+    }
 }
