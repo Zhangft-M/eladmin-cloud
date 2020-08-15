d34100567ab5c96ce24f1bb3f873b74d3f3c848d
张方涛 <767602601@qq.com>
2020-08-15 15:51:43 +0800

    测试角色信息查询之前

--- eladmin-common/eladmin-common-security/src/main/java/org/micah/security/service/UserDetailsServiceImpl.java
+++ eladmin-common/eladmin-common-security/src/main/java/org/micah/security/service/UserDetailsServiceImpl.java
@@ -22,6 +22,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import javax.validation.constraints.NotNull;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -35,41 +38,25 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
-    /**
-     * Locates the user based on the username. In the actual implementation, the search
-     * may possibly be case sensitive, or case insensitive depending on how the
-     * implementation instance is configured. In this case, the <code>UserDetails</code>
-     * object that comes back may have a username that is of a different case than what
-     * was actually requested..
-     *
-     * @param username the username identifying the user whose data is required.
-     * @return a fully populated user record (never <code>null</code>)
-     * @throws UsernameNotFoundException if the user could not be found or the user has no
-     *                                   GrantedAuthority
-     */
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
 
 
     /**
      * userfeign接口
-     *//*
+     */
     private final IRemoteUserService remoteUserService;
 
 
     private final IRemoteMenuService remoteMenuService;
 
 
-    *//**
+    /**
      * 缓存管理器，在第一次调用加载用户的方法系统会自动将其缓存
-     *//*
+     */
     private final CacheManager cacheManager;
 
-    *//**
+    /**
      * 用户登录设置
-     *//*
+     */
     private final LoginProperties loginProperties;
 
     public UserDetailsServiceImpl(IRemoteUserService remoteUserService,
@@ -81,20 +68,20 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
 
-    *//**
+    /**
      * 设置用户信息是否被缓存
      * @param enableCache
-     *//*
+     */
     public void setEnableCache(boolean enableCache) {
         this.loginProperties.setCacheEnable(enableCache);
     }
 
-    *//**
+    /**
      * 通过用户名加载用户
      * @param username
      * @return
      * @throws UsernameNotFoundException
-     *//*
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Cache cache = this.cacheManager.getCache(CacheConstants.USER_DETAILS);
@@ -114,11 +101,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
 
-    *//**
+    /**
      * 构建UserDetails
      * @param result
      * @return
-     *//*
+     */
     private UserDetails getUserDetails(ResponseEntity<SysUserDto> result) {
         if (result == null || result.getBody() == null) {
             throw new UsernameNotFoundException("用户不存在");
@@ -126,6 +113,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         SysUserDto userDto = result.getBody();
         Set<String> dbAuthsSet = new HashSet<>();
         if (CollUtil.isNotEmpty(userDto.getRoles())) {
+            userDto.getRoles().forEach(role -> {
+                dbAuthsSet.add(SecurityConstants.ROLE + role.getName());
+            });
             // 获取所有的角色
             userDto.getRoles().forEach(role -> dbAuthsSet.add(SecurityConstants.ROLE + role.getName()));
             // 根据用户的角色查询用户的菜单权限
@@ -134,7 +124,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             List<MenuDto> menuDtos = menuResult.getBody();
             if (menuDtos != null){
                 // 获取所有的权限
-                List<String> permissions = menuDtos.stream().distinct().filter(menuDto -> StringUtils.isNotBlank(menuDto.getPermission())).map(MenuDto::getPermission).collect(Collectors.toList());
+                List<String> permissions = menuDtos.stream().distinct().filter(menuDto -> StringUtils.isNotBlank(menuDto.getPermission()))
+                        .map(MenuDto::getPermission).collect(Collectors.toList());
                 dbAuthsSet.addAll(permissions);
             }
             // TODO: 2020/8/12 远程查询部门数据权限
@@ -145,5 +136,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         Collection<? extends GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(dbAuthsSet.toArray(new String[0]));
         return new LoginUser(userDto.getUsername(), userDto.getPassword(), userDto.getEnabled(),
                 true, true, true, authorities, userDto, null);
-    }*/
+
+    }
 }
+
